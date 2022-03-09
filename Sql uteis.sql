3f5ee5720a8fe1a5b4307196515aa3ddaddf4bc9
Fabricio Luis Perrone <fabricio_luis@outlook.com>
2022-03-09 15:00:06 -0800

    Atualização da lista SQL

--- Sql uteis.sql
+++ Sql uteis.sql
@@ -37,4 +37,46 @@ insert into tabela_vendedores (
 	values ('00238','Péricles Alves', 0.11,TO_DATE('21/08/2016', 'DD/MM/YYYY'),0
 	);
 	 
-	
+	select * from tabela_vendedores; // consula dados da tabela.
+	// atualiza um produto da tabela tb_produtos -> seta embalagem+nome=lata; e preço_lista=2.46 ->where todos os 
+	produtos que tem o código = '544931'.
+ update tb_produtos set
+        embalagem = 'Lata',
+        preco_lista =2.46
+    where produto = '544931';
+    
+    update tb_produtos set
+        embalagem = 'Garrafa'
+    where produto = '1078680';
+
+
+// Executando os comandos abaixo vc vair ver os itens atualizados
+SELECT * FROM TB_PRODUTOS;
+
+// executando o comando abaixo vc deleta um produto
+DELETE FROM TB_PRODUTOS
+WHERE PRODUTO = '1078680';
+
+// executando o comando abaixo vc cria uma chave primaria para a tabela produto
+ALTER TABLE TB_PRODUTOS ADD CONSTRAINT PK_TB_PRODUTOS 
+PRIMARY KEY (PRODUTO);
+
+//excutando o comando abaixo vc insere um novo cliente na tb_clientes
+INSERT INTO TB_CLIENTES (
+    CPF, NOME, ENDERECO1, ENDERECO2, 
+    BAIRRO, CIDADE, ESTADO, CEP, IDADE, 
+    SEXO, LIMITE_CREDITO, VOLUME_COMPRA, 
+    PRIMEIRA_COMPRA, DATA_NASCIMENTO
+) VALUES (
+    '00388934777', 'João da Silva', 'Rua Projetada A número 10', 
+    NULL , 'VILA ROMAN', 'TRÊS RIOS', 'RJ', '22222222', 53, 'M', 
+    20000.00, 2000, 0, TO_DATE('12/10/1965','MM/DD/YYYY')
+);
+
+//Mesmo com a tabela já criada, você pode incluir novas colunas nela, por exemplo:
+
+ALTER TABLE TB_CLIENTES ADD DATA_NASCIMENTO DATE;
+
+//Crie uma nova consulta e inclua uma chave primária na tabela de clientes:
+ALTER TABLE TB_CLIENTES ADD CONSTRAINT PK_TB_CLIENTES PRIMARY KEY (CPF);
+
